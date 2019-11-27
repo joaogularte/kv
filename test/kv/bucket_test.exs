d3b43fa04bf1e872bd5d4af0f19c9b80bca4f483
joaogularte <jov96@hotmail.com>
2019-11-27 02:19:38 -0300

    Rodado mix format

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -2,17 +2,17 @@ defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
   doctest KV.Bucket
-  
+
   setup_all do
-    IO.puts "Starting Bucket test"    
+    IO.puts("Starting Bucket test")
   end
 
   setup do
     {:ok, bucket} = KV.Bucket.start_link([])
-    %{bucket: bucket}  
+    %{bucket: bucket}
   end
 
-  test "stores values by key", %{bucket: bucket } do
+  test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
     KV.Bucket.put(bucket, "milk", 1)
     assert KV.Bucket.get(bucket, "milk") == 1
