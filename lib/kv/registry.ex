78e5d63aa7021edcf2624071cf04476a1a28a13c
joaogularte <jov96@hotmail.com>
2019-12-28 16:13:31 -0300

    Reforço sobre GenServers

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -1,81 +1,60 @@
 defmodule KV.Registry do
     use GenServer
-    @moduledoc """
-        Providers a set of functions that implement GenServer client and server 
-    """ 
-    #Implementation client
+
+    ##Server Callback
+    @impl true
+    def init(:ok) do
+        {:ok, %{}}
+    end
+
+    @impl true
+    def handle_call({:lookup, name}, _from, state) do
+        {:reply, Map.fetch(state, name), state}
+    end
+
+    def handle_call(:all, _from, state) do
+        {:reply, state, state}
+    end
+    
+    @impl true
+    def handle_cast({:create, name}, state) do
+        if Map.has_key?(state, name) do
+            {:noreply, state}
+        else
+            {:ok, bucket } = KV.Bucket.start_link([])
+            new_state = Map.put(state, name, bucket)
+            {:noreply, new_state}
+        end
+    end
+
+
+    ## Client API
     @doc """
-    Starts a registry
+    Start the registry
     """
     def start_link(opts) do
         GenServer.start_link(__MODULE__, :ok, opts)
     end
 
     @doc """
-    Ensures there is a bucket associated with the given `name` in `server`     
-    """
-    def create(server, name) do
-        GenServer.cast(server, {:create, name})
-    end
-    @doc """
-    Returns {:ok, pid} if the bucket exists, `:error` otherwise
+    Looks up the bucket for `name` stored in `server`
+    Returns `{:ok, pid}` if the bucket exists, `:error` otherwise
     """
     def lookup(server, name) do
         GenServer.call(server, {:lookup, name})
     end
 
-    #Implementation server
     @doc """
-    Handle the start_link requests
+    Returns all buckets stored in `server`
     """
-    @impl true
-    def init(:ok) do
-        names = %{}
-        refs = %{}
-        {:ok, {names, refs}}
+    def all(server) do
+        GenServer.call(server, :all)
     end
 
     @doc """
-    Handle the call requests
+    Ensures there is a bucket associated with given `name` in `server`. 
     """
-    @impl true
-    def handle_call({:lookup, name}, _from, state) do
-        {names, _} = state
-        {:reply, Map.fetch(names, name), state}
-    end
-    
-
-    @doc """
-    Handle the call requests
-    """
-    @impl true
-    def handle_cast({:create, name}, state) do
-        {names, refs} = state
-        if Map.has_key?(names, name) do
-            {:noreply, names}
-        else
-            {:ok, bucket} = KV.Bucket.start_link([])
-            ref = Process.monitor(bucket)
-            refs = Map.put(refs, ref, name)
-            names = Map.put(names, name, bucket)
-            {:noreply, {names, refs}}
-        end 
-    end
-    
-    @doc """
-    
-    """
-    @impl true
-    def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
-        {name, refs} = Map.pop(refs, ref)
-        names = Map.delete(names, name)
-        {:noreply, {names, refs}}
-    end
-    @doc """
-    
-    """
-    @impl true
-    def handle_info(_msg, state) do
-        {:noreply, state}
+    def create(server, name) do
+        GenServer.cast(server, {:create, name})
     end
 end
