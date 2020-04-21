b2bee298e10924025facd7bb912bab622a46f450
joaogularte <jov96@hotmail.com>
2020-04-21 16:34:10 -0300

    refact: move server callback to server module

---
+++ lib/kv/registry/server.ex
@@ -0,0 +1,42 @@
+defmodule KV.Registry.Server do
+    
+    def init(:ok) do
+        names = %{}
+        refs = %{}
+        {:ok, {names, refs}}
+    end
+
+    def handle_call({:lookup, name}, _from, state) do
+        {names, _} = state
+        {:reply, Map.fetch(names, name), state}
+    end
+
+    def handle_call(:all, _from, state) do
+        {names, _} = state
+        {:reply, names, state}
+    end
+
+    def handle_cast({:create, name}, {names, refs}) do
+        if Map.has_key?(names, name) do
+            {:noreply, {names, refs}}
+        else
+            {:ok, bucket } = KV.Bucket.start_link([])
+            ref_monitor = Process.monitor(bucket)
+            new_refs = Map.put(refs, ref_monitor, name)
+            new_names = Map.put(names, name, bucket)
+            new_state = {new_names, new_refs}
+            {:noreply, new_state}
+        end
+    end
+
+    def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+        {name, refs} = Map.pop(refs, ref)
+        names = Map.delete(names, name)
+        {:noreply, {names, refs}}
+    end
+
+    def handle_info(_msg, state) do
+        {:noreply, state}
+    end
+
+end
