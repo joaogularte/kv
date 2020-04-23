248d3cee8d1a3477f03b49f5d302b651551c4158
joaogularte <jov96@hotmail.com>
2020-04-23 20:54:28 -0300

    test: made test pass

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -1,42 +1,51 @@
 defmodule KV.RegistryTest do
-    use ExUnit.Case, async: true
+  use ExUnit.Case, async: true
 
-    doctest KV.Registry
+  doctest KV.Registry
 
-    setup_all do 
-        IO.puts("Starting Registry test")
-    end
+  setup_all do
+    IO.puts("Starting Registry test")
+  end
 
-    setup do
-        registry = start_supervised!(KV.Registry)
-        %{registry: registry}
-    end
+  setup do
+    registry =
+      start_supervised!(%{
+        id: KV.Registry,
+        start: {
+          KV.Registry,
+          :start_link,
+          [[]]
+        }
+      })
 
-    test "spaws buckets", %{registry: registry} do
-        assert KV.Registry.lookup(registry, "shopping") == :error
-        KV.Registry.create(registry, "shopping")
-        assert {:ok, shopping_bucket} = KV.Registry.lookup(registry, "shopping")
+    %{registry: registry}
+  end
 
-        KV.Bucket.put(shopping_bucket, "apple", 3)
-        assert KV.Bucket.get(shopping_bucket, "apple") == 3
-    end
+  test "spaws buckets", %{registry: registry} do
+    assert KV.Registry.lookup(registry, "shopping") == :error
+    KV.Registry.create(registry, "shopping")
+    assert {:ok, shopping_bucket} = KV.Registry.lookup(registry, "shopping")
 
-    test "get all buckets", %{registry: registry} do
-        KV.Registry.create(registry, "shopping")
-        KV.Registry.create(registry, "market")
-        assert KV.Registry.all_buckets(registry)
-    end
+    KV.Bucket.put(shopping_bucket, "apple", 3)
+    assert KV.Bucket.get(shopping_bucket, "apple") == 3
+  end
 
-    test "get all refs buckets", %{registry: registry} do
-        KV.Registry.create(registry, "shopping")
-        KV.Registry.create(registry, "shopping")
-        assert KV.Registry.all_refs(registry)        
-    end
+  test "get all buckets", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    KV.Registry.create(registry, "market")
+    assert KV.Registry.all_buckets(registry)
+  end
 
-    test "removes buckets on exit", %{registry: registry} do
-        KV.Registry.create(registry, "shopping")
-        {:ok, bucket_shopping} = KV.Registry.lookup(registry, "shopping")
-        Agent.stop(bucket_shopping)
-        assert KV.Registry.lookup(registry, "shopping") == :error
-    end
+  test "get all refs buckets", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    KV.Registry.create(registry, "shopping")
+    assert KV.Registry.all_refs(registry)
+  end
+
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket_shopping} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket_shopping)
+    assert KV.Registry.lookup(registry, "shopping") == :error
+  end
 end
